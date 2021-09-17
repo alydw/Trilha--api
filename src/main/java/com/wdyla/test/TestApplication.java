aa174853da4ea482c5203befe4eee22c1981ecfd
alydw <wdyla2001@hotmail.com>
2021-09-16 21:33:06 -0300

    perfil de teste implementado

--- src/main/java/com/wdyla/test/TestApplication.java
+++ src/main/java/com/wdyla/test/TestApplication.java
@@ -1,36 +1,17 @@
 package com.wdyla.test;
 
-import java.util.Arrays;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.wdyla.test.domain.Categoria;
-import com.wdyla.test.domain.Livro;
-import com.wdyla.test.repositories.CategoriaRepository;
-import com.wdyla.test.repositories.LivroRepository;
 
 @SpringBootApplication
-public class TestApplication implements CommandLineRunner{
+public class TestApplication {
 	
-	@Autowired
-	private CategoriaRepository categoriaRepository;
-	@Autowired
-	private LivroRepository livroRepository;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
 	}
-	@Override
-	public void run(String... args) throws Exception{
-		Categoria cat1 = new Categoria(null, "Informática", "Livro de TI");
-		Livro l1 = new Livro(null, "Clean code", "Robert Martin", "Lorem ipsum", cat1);
-		
-		cat1.getLivros().addAll(Arrays.asList(l1));
-		
-		this.categoriaRepository.saveAll(Arrays.asList(cat1));
-		this.livroRepository.saveAll(Arrays.asList(l1));
-	}
+	
 
 }
