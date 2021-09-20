2a35c18c9632715394e3e67d9f34b2e9b7ec423b
alydw <wdyla2001@hotmail.com>
2021-09-20 16:18:29 -0300

    POST para categoria

--- src/main/java/com/wdyla/test/resources/CategoriaResource.java
+++ src/main/java/com/wdyla/test/resources/CategoriaResource.java
@@ -1,6 +1,7 @@
 package com.wdyla.test.resources;
 
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.wdyla.test.domain.Categoria;
 import com.wdyla.test.dtos.CategoriaDTO;
@@ -36,7 +40,25 @@ public class CategoriaResource {
 		new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 	}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> create(@RequestBody Categoria obj) {
+		obj = service.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();
+	}
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
