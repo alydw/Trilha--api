7eff31fb0bd2d19ea858b8d6a611c6c151586171
alydw <wdyla2001@hotmail.com>
2021-09-21 11:07:11 -0300

    Listando os livros por categoria

--- src/main/java/com/wdyla/test/resources/LivroResource.java
+++ src/main/java/com/wdyla/test/resources/LivroResource.java
@@ -1,13 +1,18 @@
 package com.wdyla.test.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wdyla.test.domain.Livro;
+import com.wdyla.test.dtos.LivroDTO;
 import com.wdyla.test.service.LivroService;
 
 @RestController
@@ -23,4 +28,25 @@ public class LivroResource {
 		return ResponseEntity.ok().body(obj);
 		
 	}
+	
+	@GetMapping
+	public ResponseEntity<List<LivroDTO>> findAll(@RequestParam(value = "categoria",
+	defaultValue = "0") Integer id_cat){
+		List<Livro> list = service.findAll(id_cat);
+		List<LivroDTO> listDTO = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
